b88a40e3008359d504c745de2d5a4f53abf26450
Jon <47305276+Captain-Seli@users.noreply.github.com>
2023-04-15 12:29:31 +0900

    Initial Commit

---
+++ Parser.py
@@ -0,0 +1,36 @@
+
+import PyPDF2
+
+def parse_pdf():
+    # Prompt the user for the PDF file path
+    pdf_path = input("Enter the path to the PDF file: ")
+
+    # Open the PDF file in read mode
+    try:
+        with open(pdf_path, "rb") as pdf_file:
+            # Create a PDF reader object
+            pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+
+            # Get the number of pages in the PDF document
+            num_pages = pdf_reader.getNumPages()
+
+            # Loop through each page and extract the text
+            for page_num in range(num_pages):
+                page = pdf_reader.getPage(page_num)
+                text = page.extractText()
+                print(text)
+
+    except FileNotFoundError:
+        print(f"Error: Could not find file {pdf_path}")
+    except PermissionError:
+        print(f"Error: Permission denied for file {pdf_path}")
+
+def main():
+    # Call the parse_pdf_file method
+    pdf_text = parse_pdf()
+
+    # Print the parsed text
+    print(pdf_text)
+
+if __name__ == '__main__':
+    main()
